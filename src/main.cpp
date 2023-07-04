c1a25dc2c87d6a6e58d110c431870f8cde7fd4f5
kaushikbaidya09 <kaushikbaidya02@gmail.com>
2023-07-04 18:40:11 +0530

    Updated Mqtt Server

--- src/main.cpp
+++ src/main.cpp
@@ -1,112 +1,105 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+#include <PubSubClient.h>
 
-/************************* WiFi Access Point *********************************/
+// WiFi
+const char *ssid = "Restricted Connection";   // Your WiFi name
+const char *password = "24681355";            // Your WiFi password
 
-#define WLAN_SSID       "xxxxxxxx"
-#define WLAN_PASS       "xxxxxxxx"
+// MQTT Broker
+const char *mqtt_broker = "dfe88d08.ala.us-east-1.emqxsl.com"; // broker address
+const char *mqtt_username = "User01"; // username for authentication
+const char *mqtt_password = "Play#KB05"; // password for authentication
+const int mqtt_port = 8883; // port of MQTT over TCP
+const char *client_id = "KBs_Device_01";
 
-/************************* Adafruit.io Setup *********************************/
+// Publish
+const char *mqtt_topic_pub_01 = "pub01";
+// Subscribe
+const char *mqtt_topic_sub_01 = "sub01";
 
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME  "xxxxxxxx"
-#define AIO_KEY       "xxxxxxxx"
+// init wifi client
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
 
-/**************************** IO Pin Setup ***********************************/
+/*
+  The common fingerprints of EMQX broker, for reference only.
+  If you are not using EMQX Cloud Serverless or public EMQX broker,
+  you need to calculate the sha1 fingerprint of your server certificate
+  and update the 'fingerprint' variable below.
+*/
+// 1. fingerprint of public emqx broker. Host: broker.emqx.io
+// const char* fingerprint = "B6 C6 FF 82 C6 59 09 BB D6 39 80 7F E7 BC 10 C9 19 C8 21 8E";
+// 2. fingerprint of EMQX Cloud Serverless. Host: *.emqxsl.com
+const char* fingerprint = "42:AE:D8:A3:42:F1:C4:1F:CD:64:9C:D7:4B:A1:EE:5B:5E:D7:E2:B5";
+// 3. fingerprint of EMQX Cloud Serverless. Host: *.emqxsl.cn
+// const char* fingerprint = "7E:52:D3:84:48:3C:5A:9F:A4:39:9A:8B:27:01:B1:F8:C6:AD:D4:47";
 
-int Bed_light = D5;
-int Night_lamp = D6;
-
-/************ Global State (you don't need to change this!) ******************/
-
-WiFiClient client;
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-
-/****************************** Feeds ***************************************/
-
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/xxxx");
-
-/*************************** Sketch Code ************************************/
-
-void MQTT_connect();
+void callback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
+  // Set software serial baud to 115200;
   Serial.begin(9600);
-
-  pinMode(Bed_light,OUTPUT);
-  pinMode(Night_lamp,OUTPUT);
-
-  Serial.println(F("Adafruit MQTT"));
-
-  // Connect to WiFi access point.
-  Serial.println(); Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
-
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  // connecting to a WiFi network
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
-  Serial.println();
+  Serial.println("Connected to the WiFi network");
 
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
-
-  // Setup MQTT subscription for onoff feed.
-  mqtt.subscribe(&onoffbutton);
+  // connecting to a mqtt broker
+  espClient.setFingerprint(fingerprint);
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    Serial.printf("The client %s connects to the mqtt broker\n", client_id);
+    if (client.connect(client_id, mqtt_username, mqtt_password)) {
+        Serial.println("Connected to MQTT broker.");
+        client.subscribe(mqtt_topic_sub_01);
+    } else {
+        Serial.print("Failed to connect to MQTT broker, rc=");
+        Serial.print(client.state());
+        Serial.println(" Retrying in 5 seconds.");
+        delay(5000);
+    }
+  }
+  client.publish(mqtt_topic_pub_01, "Hi EMQX I'm KB9520");
 }
 
-uint32_t x=0;
+String message = "";
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    message = "";
+    for (int i = 0; i < length; i++) {
+        message = "" + ((String) payload[i]);
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+    Serial.print("Variable Massage: ");
+    Serial.println(message);
+    Serial.println("-----------------------");
+}
 
-void loop() {
-  MQTT_connect();
-
-/********************* MQTT SUBSCRIBE STUFF ******************************/
-
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(5000))) {
-    if (subscription == &onoffbutton) {
-      Serial.print(F("Got: "));
-      Serial.println((char *)onoffbutton.lastread);
-
-      if (strcmp((char *)onoffbutton.lastread, "ON") == 0) {
-        Serial.println("Bed Light ON"); 
-        digitalWrite(Bed_light, HIGH);
-      }
-      if (strcmp((char *)onoffbutton.lastread, "OFF") == 0) {
-        Serial.println("Bed Light OFF");
-        digitalWrite(Bed_light,LOW);
-      }
+void reconnect() {
+  while (!client.connected()) {
+    Serial.println("Reconnecting to MQTT broker...");
+    if (client.connect(client_id, mqtt_username, mqtt_password)) {
+        Serial.println("Reconnected to MQTT broker.");
+        client.subscribe(mqtt_topic_sub_01);
+    } else {
+        Serial.print("Failed to reconnect to MQTT broker, rc=");
+        Serial.print(client.state());
+        Serial.println("Retrying in 5 seconds.");
+        delay(5000);
     }
   }
 }
 
-void MQTT_connect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
+void loop() {
+  if (!client.connected()) {
+    reconnect();
   }
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         ESP.reset();
-         //while (1);
-       }
-  }
-  Serial.println("MQTT Connected!");
+  client.loop();
 }
